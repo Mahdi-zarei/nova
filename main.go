d458292315e74e08f950db75e0b59469617a8240
Mahdi <mahdi.zrei@gmail.com>
2023-03-15 02:03:26 +0330

    adds flag for lim

--- main.go
+++ main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 )
 
 var lg *log.Logger
+var lim string
 
 func main() {
+	flag.StringVar(&lim, "lim", "200", "count of conns per 5 sec")
+	flag.Parse()
+
+	limInt, err := strconv.Atoi(lim)
+	if err != nil {
+		panic(err)
+	}
+
 	counter := map[string]int32{}
 	syncer := sync.Mutex{}
 	lg = log.New(os.Stdout, "", log.Ltime)
 
-	lg.Printf("sfsadja %s", "sda")
+	lg.Println("Starting service with lim %v", limInt)
 
 	server, err := net.ListenTCP("tcp", &net.TCPAddr{
 		Port: 6543,
@@ -32,7 +43,7 @@ func main() {
 		case <-ticker:
 			syncer.Lock()
 			for key, val := range counter {
-				val -= 1000
+				val -= int32(limInt)
 
 				if val < 0 {
 					delete(counter, key)
@@ -51,7 +62,7 @@ func main() {
 			continue
 		}
 		conn.SetNoDelay(true)
-		lg.Printf("Incoming connection from %s", conn.RemoteAddr().String())
+		lg.Printf("Incoming connection from [%s]", conn.RemoteAddr().String())
 
 		go func() {
 			addr := conn.RemoteAddr()
@@ -63,9 +74,9 @@ func main() {
 			if !ok {
 				v = 0
 			}
-			lg.Printf("Current count for %s is %v", addr.String(), v)
+			lg.Printf("Current count for [%s] is [%v]", addr.String(), v)
 			v++
-			if v > 1000 {
+			if v > int32(limInt) {
 				allowed = false
 			}
 			counter[ip] = v
@@ -88,7 +99,7 @@ func forwardConnection(src net.Conn) {
 
 	done := make(chan struct{})
 
-	lg.Printf("starting relay from %s to %s", src.RemoteAddr().String(), dst.RemoteAddr().String())
+	lg.Printf("starting relay from [%s] to [%s]", src.RemoteAddr().String(), dst.RemoteAddr().String())
 
 	go func() {
 		defer src.Close()
