b22d48b108b6188076ca2925927383014ecd42bc
Mahdi <mahdi.zrei@gmail.com>
2023-03-15 20:08:19 +0330

    better spam handling

--- main.go
+++ main.go
@@ -14,11 +14,13 @@ import (
 
 var lg *log.Logger
 var lim string
+var threshold int32
 
 func main() {
 	flag.StringVar(&lim, "lim", "200", "count of conns per 5 sec")
 	flag.Parse()
 
+	threshold = 1000
 	limInt, err := strconv.Atoi(lim)
 	if err != nil {
 		panic(err)
@@ -46,6 +48,9 @@ func main() {
 			case <-ticker:
 				syncer.Lock()
 				for key, val := range counter {
+					if val > threshold {
+						continue
+					}
 					val -= int32(limInt)
 
 					if val < 0 {
@@ -78,12 +83,17 @@ func main() {
 			if !ok {
 				v = 0
 			}
-			lg.Printf("current count for [%s] is [%v]", ip, v)
-			v++
-			if v > int32(limInt) {
+
+			if v > threshold {
 				allowed = false
+			} else {
+				lg.Printf("current count for [%s] is [%v]", ip, v)
+				v++
+				if v > int32(limInt) {
+					allowed = false
+				}
+				counter[ip] = v
 			}
-			counter[ip] = v
 			syncer.Unlock()
 
 			if allowed {
@@ -91,12 +101,14 @@ func main() {
 				totalCounter++
 				lg.Printf("total connection count: [%v]", totalCounter)
 				totMu.Unlock()
+
 				forwardConnection(conn)
+
+				lg.Printf("closing connection for [%s]", ip)
+				totMu.Lock()
+				totalCounter--
+				totMu.Unlock()
 			}
-			lg.Printf("closing connection for [%s]", ip)
-			totMu.Lock()
-			totalCounter--
-			totMu.Unlock()
 			conn.Close()
 		}()
 	}
