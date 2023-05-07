b9018c88df7d63130dd627433813c4ccedc46948
Mahdi <mahdi.zrei@gmail.com>
2023-05-07 04:11:42 +0330

    init server

--- main.go
+++ main.go
@@ -1,171 +1,254 @@
 package main
 
 import (
-	"flag"
-	"io"
+	"errors"
+	"github.com/google/uuid"
 	"log"
 	"net"
 	"os"
 	"strconv"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-var lg *log.Logger
-var lim string
-var portString string
-var threshold int32
+var ender []byte
+var linker map[string]map[int]*net.TCPConn
+var locker sync.Mutex
+var destIP string
+var destPort int
+var logger *log.Logger
+var connCount int
+var bufferSize int
+var totalCount atomic.Int32
 
 func main() {
-	flag.StringVar(&lim, "lim", "200", "count of conns per 5 sec")
-	flag.StringVar(&portString, "port", "6543", "port")
-	flag.Parse()
+	destIP = "188.121.115.36"
+	destPort = 6543
+	logger = log.New(os.Stdout, "", log.LstdFlags)
+	bufferSize = 512 * 1024
+	connCount = 4
+	linker = make(map[string]map[int]*net.TCPConn)
+	var tmp []string
+	tmp = []string{"\000", "\002", "\004", "\000"}
+	for _, x := range tmp {
+		ender = append(ender, []byte(x)...)
+	}
 
-	threshold = 100
-	limInt, err := strconv.Atoi(lim)
+	srv, err := net.ListenTCP("tcp", &net.TCPAddr{Port: 5555})
 	if err != nil {
 		panic(err)
 	}
-
-	port, err := strconv.Atoi(portString)
-	if err != nil {
-		panic(err)
-	}
-
-	counter := map[string]int32{}
-	blackList := map[string]struct{}{}
-	blackList["198.244.191.140"] = struct{}{}
-	syncer := sync.Mutex{}
-	totMu := sync.Mutex{}
-	totalCounter := 0
-	lg = log.New(os.Stdout, "", log.Ltime)
-
-	lg.Printf("starting service with lim %v", limInt)
-
-	server, err := net.ListenTCP("tcp", &net.TCPAddr{
-		Port: port,
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	go func() {
-		ticker := time.Tick(5 * time.Second)
-		for {
-			select {
-			case <-ticker:
-				syncer.Lock()
-				for key, val := range counter {
-
-					val -= int32(limInt)
-
-					if val < 0 {
-						delete(counter, key)
-						continue
-					}
-
-					counter[key] = val
-				}
-				syncer.Unlock()
-			}
-		}
-	}()
 
 	for {
-		conn, err := server.AcceptTCP()
+		conn, err := srv.AcceptTCP()
 		if err != nil {
+			logger.Printf("error in accepting connection: %s", err)
 			continue
 		}
-		addr := conn.RemoteAddr()
-		ip := strings.Split(addr.String(), ":")[0]
+		conn.SetNoDelay(true)
+		conn.SetKeepAlive(true)
+		conn.SetKeepAlivePeriod(1 * time.Second)
 
-		if _, is := blackList[ip]; is {
+		uid, seq, err := getIdentifier(conn)
+		if err != nil {
+			logger.Printf("error in getting identifying data: %s", err)
 			conn.Close()
 			continue
 		}
 
-		go func() {
-			lg.Printf("Incoming connection from [%s]", addr.String())
-			conn.SetNoDelay(true)
-
-			allowed := true
-			syncer.Lock()
-			v, ok := counter[ip]
-			if !ok {
-				v = 0
-			}
-
-			if v > threshold {
-				lg.Printf("blacklisted %s", ip)
-				blackList[ip] = struct{}{}
-				allowed = false
-			} else {
-				lg.Printf("current count for [%s] is [%v]", ip, v)
-				v++
-				if v > int32(limInt) {
-					allowed = false
-				}
-				counter[ip] = v
-			}
-			syncer.Unlock()
-
-			if allowed {
-				totMu.Lock()
-				totalCounter++
-				lg.Printf("total connection count: [%v]", totalCounter)
-				totMu.Unlock()
-
-				forwardConnection(conn)
-
-				lg.Printf("closing connection for [%s]", ip)
-				totMu.Lock()
-				totalCounter--
-				totMu.Unlock()
-			}
-			conn.Close()
-		}()
+		if mapConnection(uid, seq, conn) == connCount {
+			logger.Printf("connections are all here, starting to forward")
+			go handleConnections(uid, linker[uid])
+		}
 	}
 }
 
-func forwardConnection(src *net.TCPConn) {
-	dst, err := net.DialTCP("tcp", nil, &net.TCPAddr{Port: 888})
+func handleConnections(uid string, conns map[int]*net.TCPConn) {
+	upstream, err := net.DialTCP("tcp", nil, &net.TCPAddr{
+		IP:   net.ParseIP(destIP),
+		Port: destPort,
+	})
 	if err != nil {
+		cleanUpConnections(uid)
+		logger.Printf("error dialing the upstream server: %s", err)
+		return
+	}
+	upstream.SetNoDelay(true)
+	upstream.SetKeepAlive(true)
+	upstream.SetKeepAlivePeriod(1 * time.Second)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer cleanUpConnections(uid)
+		defer upstream.Close()
+		handleManyToOneForward(upstream, conns)
+		wg.Done()
+	}()
+
+	go func() {
+		defer cleanUpConnections(uid)
+		defer upstream.Close()
+		handleOneToManyForward(upstream, conns)
+		wg.Done()
+	}()
+
+	totalCount.Add(1)
+	logger.Printf("starting forward for new client, count: %v", totalCount.Load())
+
+	wg.Wait()
+	totalCount.Add(-1)
+	logger.Printf("forward stopped, client count: %v", totalCount.Load())
+}
+
+func handleManyToOneForward(dest *net.TCPConn, conns map[int]*net.TCPConn) {
+	buffer := make([]byte, bufferSize)
+	cnt := 0
+	for {
+		nr, err := conns[cnt].Read(buffer)
+		if nr > 0 {
+
+			if isComplete(buffer, nr) {
+				nr = nr - 4
+				cnt++
+				cnt %= connCount
+			}
+
+			logger.Println("read ", cnt)
+			_, err2 := dest.Write(buffer[:nr])
+			if err2 != nil {
+				logger.Printf("error in writing to openvpn connection: %s", err2)
+				return
+			}
+
+		}
+
+		if err != nil {
+			logger.Printf("error in reading from mux connections: %s", err)
+			return
+		}
+	}
+}
+
+func handleOneToManyForward(src *net.TCPConn, conns map[int]*net.TCPConn) {
+	buffer := make([]byte, connCount*bufferSize)
+	cnt := 0
+	for {
+		nr, err := src.Read(buffer)
+
+		if nr > 0 {
+
+			buffer, nr = appendToBuffer(buffer, nr)
+
+			logger.Println("write ", cnt)
+			_, err2 := conns[cnt].Write(buffer[:nr])
+			if err2 != nil {
+				logger.Printf("error in writing to mux connections: %s", err2)
+				return
+			}
+
+			cnt++
+			cnt %= connCount
+
+		}
+
+		if err != nil {
+			logger.Printf("error in reading from openvpn connection: %s", err)
+			return
+		}
+	}
+}
+
+func cleanUpConnections(uid string) {
+	locker.Lock()
+	defer locker.Unlock()
+	conns, ok := linker[uid]
+	if !ok {
 		return
 	}
 
-	src.SetKeepAlive(true)
-	src.SetKeepAlivePeriod(2 * time.Second)
+	for _, conn := range conns {
+		conn.Close()
+	}
+	delete(linker, uid)
+}
 
-	dst.SetKeepAlive(true)
-	dst.SetKeepAlivePeriod(2 * time.Second)
+func getIdentifier(conn *net.TCPConn) (string, int, error) {
+	data := ""
+	buffer := make([]byte, 1024)
+	fullRead := 0
 
-	dst.SetNoDelay(true)
-
-	done := make(chan struct{})
-
-	lg.Printf("starting relay from [%s] to [%s]", src.RemoteAddr().String(), dst.RemoteAddr().String())
-
-	go func() {
-		defer src.Close()
-		defer dst.Close()
-		_, err := io.Copy(dst, src)
-		if err != nil {
-			lg.Printf("Forward from dst to src stopped with [%s]", err)
+	for {
+		nr, err := conn.Read(buffer)
+		fullRead += nr
+		if nr > 0 {
+			if isComplete(buffer, nr) {
+				data += convertToString(buffer, nr-4)
+				spl := strings.Split(data, "#")
+				uid, err := uuid.Parse(spl[0])
+				if err != nil {
+					return "", 0, err
+				}
+				seq, err := strconv.Atoi(spl[1])
+				if err != nil {
+					return "", 0, err
+				}
+				return uid.String(), seq, nil
+			}
+			data += convertToString(buffer, nr)
 		}
-		done <- struct{}{}
-	}()
-
-	go func() {
-		defer src.Close()
-		defer dst.Close()
-		_, err := io.Copy(src, dst)
 		if err != nil {
-			lg.Printf("Forward from src to dst stopped with [%s]", err)
+			return "", 0, err
 		}
-		done <- struct{}{}
-	}()
+		if fullRead > 150 {
+			return "", 0, errors.New("unknown init packet format")
+		}
+	}
+}
 
-	<-done
-	<-done
+func convertToString(buffer []byte, nr int) string {
+	res := ""
+	for idx, b := range buffer {
+		if idx >= nr {
+			break
+		}
+
+		res += string(b)
+	}
+	return res
+}
+
+func isComplete(buffer []byte, nr int) bool {
+	if nr >= len(ender) {
+
+		for i := 0; i < len(ender); i++ {
+			if buffer[nr-len(ender)+i] != ender[i] {
+				return false
+			}
+		}
+
+		return true
+	}
+	return false
+}
+
+func appendToBuffer(buffer []byte, nr int) ([]byte, int) {
+	for i := 0; i < len(ender); i++ {
+		buffer[nr+i] = ender[i]
+	}
+
+	return buffer, nr + len(ender)
+}
+
+func mapConnection(uid string, seq int, conn *net.TCPConn) int {
+	locker.Lock()
+	defer locker.Unlock()
+	if _, ok := linker[uid]; !ok {
+		linker[uid] = make(map[int]*net.TCPConn)
+	}
+	linker[uid][seq] = conn
+
+	return len(linker[uid])
 }
